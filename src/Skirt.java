845165650fbfce0f7610781761476dc14b9b061b
zehraaqyol <zehraaqyol@gmail.com>
2022-03-04 14:48:34 +0300

    work3

--- src/Skirt.java
+++ src/Skirt.java
@@ -3,6 +3,7 @@ public class Skirt {
     public static void main(String[] args) {
         System.out.println("BE COOL");
         System.out.println("keep your smile");
+        System.out.println("imagine and walk away");
 
 
     }
